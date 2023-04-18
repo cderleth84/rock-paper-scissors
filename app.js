cdda6ae680991d56293cdda24b1c98badc3e969d
Chris <cderleth84@me.com>
2023-04-17 22:34:29 -0700

    Fixed logic, now game works greatgit status

--- app.js
+++ app.js
@@ -24,43 +24,38 @@ let computerChoice;
 let result;
 
 function playGame(userChoice, computerChoice) {
-  function playerChoice(playerChoice) {
-    userChoice = playerChoice;
-    document.getElementById("user-choice").innerHTML = userChoice;
-    return userChoice;
-  }
-  playerChoice(userChoice);
+  document.getElementById("user-choice").innerHTML = userChoice;
 
   function computerSelector(randomNumber) {
     if (randomNumber === 0) {
-      computerChoice = "rock";
+      computerChoice = "Rock";
       document.getElementById("computer-choice").innerHTML = computerChoice;
       return computerChoice;
     } else if (randomNumber === 1) {
-      computerChoice = "paper";
+      computerChoice = "Paper";
       document.getElementById("computer-choice").innerHTML = computerChoice;
       return computerChoice;
     } else {
-      computerChoice = "scissors";
+      computerChoice = "Scissors";
       document.getElementById("computer-choice").innerHTML = computerChoice;
       return computerChoice;
     }
   }
   computerSelector(randomNumber);
 
-  if (userChoice == computerChoice) {
+  if (userChoice === computerChoice) {
     result = "Tie";
     document.getElementById("result").innerHTML = result;
     return result;
-  } else if (userChoice == "Rock" && computerChoice == "Paper") {
+  } else if (userChoice === "Rock" && computerChoice === "Paper") {
     result = "Computer Wins";
     document.getElementById("result").innerHTML = result;
     return result;
-  } else if (userChoice == "Paper" && computerChoice == "Scissors") {
+  } else if (userChoice === "Paper" && computerChoice === "Scissors") {
     result = "Computer Wins";
     document.getElementById("result").innerHTML = result;
     return result;
-  } else if (userChoice == "Scissors" && computerChoice == "Rock") {
+  } else if (userChoice === "Scissors" && computerChoice === "Rock") {
     result = "Computer Wins";
     document.getElementById("result").innerHTML = result;
     return result;
@@ -76,3 +71,11 @@ function playGame(userChoice, computerChoice) {
 //   console.log("Playing Again");
 // }
 // console.log("Not playing again");
+
+// switch (key) {
+//   case value:
+//     break;
+
+//   default:
+//     break;
+// }
